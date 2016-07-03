edb95690e4c0caa2c7189a84a63c0b3a22f58530
Mateusz Derks <mateusz.derks@toptal.com>
2016-07-03 09:23:50 +0200

    Cleanup unused code

--- example/index.js
+++ example/index.js
@@ -35554,33 +35554,7 @@ exports.default = AceEditorWidget;
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = applyParams;
 exports.default = applyParam;
-function applyParams(editor$, params$) {
-  return editor$.flatMap(function (editor) {
-    return params$.reduce(function (editor, config) {
-      var key = config[0];
-      var value = config[1];
-
-      switch (key) {
-        case 'theme':
-          editor.setTheme(value);
-          break;
-        case 'mode':
-          editor.session.setMode(value);
-          break;
-        case 'readOnly':
-          editor.setReadOnly(value);
-          break;
-
-        default:
-          throw new Error('Unrecognized configuration key: ' + key + ', use `editor$` sink and handle it on your own');
-      }
-      return editor;
-    }, editor);
-  });
-}
-
 function applyParam(editor, key, value) {
   switch (key) {
     case 'theme':
--- src/apply_param.js
+++ src/apply_param.js
@@ -1,29 +1,3 @@
-export default function applyParams(editor$, params$) {
-  return editor$
-    .flatMap(editor => {
-      return params$.reduce((editor, config) => {
-        const key = config[0]
-        const value = config[1]
-
-        switch(key) {
-          case 'theme':
-            editor.setTheme(value)
-            break
-          case 'mode':
-            editor.session.setMode(value)
-            break
-          case 'readOnly':
-            editor.setReadOnly(value)
-            break
-
-          default:
-            throw new Error('Unrecognized configuration key: ' + key + ', use `editor$` sink and handle it on your own')
-        }
-        return editor
-      }, editor)
-    })
-}
-
 export default function applyParam(editor, key, value) {
   switch(key) {
     case 'theme':
